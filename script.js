e5124f8c747cb3bdb76e115fec591c5b6577620f
ThomasTW935 <thomastw935@gmail.com>
2021-01-17 03:06:49 +0800

    Script flip animation

--- script.js
+++ script.js
@@ -15,15 +15,9 @@ function CountdownUpdate(date) {
     let labelDays = document.querySelector('#days')
     let labelHours = document.querySelector('#hours')
     let labelMinutes = document.querySelector('#minutes')
-    let labelSeconds = document.querySelector('#seconds')
+    let cardSeconds = document.querySelector('#seconds')
 
-    labelDays.innerHTML = days
-    labelHours.innerHTML = hours
-    labelMinutes.innerHTML = minutes
-    labelSeconds.innerHTML = seconds
-
-
-    // labelSeconds
+    CardAnimation(cardSeconds, seconds)
 
     if (distance < 0) {
       clearInterval(x)
@@ -34,3 +28,45 @@ function CountdownUpdate(date) {
 let countDownDelay = 1000 * 60 * 60 * 24 * 7
 let countDownDate = new Date().getTime() + countDownDelay;
 CountdownUpdate(countDownDate)
+
+async function CardAnimation(card, seconds) {
+  // let cardDays = document.querySelector('#days')
+  let cardMid = card.childNodes[1]
+  let cardTop = card.childNodes[3]
+  let cardBot = card.childNodes[5]
+  let cardTopSpan = cardTop.childNodes[0]
+  let cardBotSpan = cardBot.childNodes[0]
+
+  // Style change
+
+  StyleChange([cardMid, cardTop, cardBot])
+
+  // Count change
+  await timer(500)
+  RemoveClass([cardMid, cardTop, cardBot])
+  await timer(500)
+  CountChange([cardMid, cardTopSpan, cardBotSpan], seconds)
+
+}
+function StyleChange(elements) {
+  elements.forEach(element => {
+    // element.style.opacity = '0';
+    // if (!element.classList.contains('countdown__Card--Middle')) {
+    //   element.style.transform = 'rotateX(180deg)';
+    // }
+    element.classList.add('countdown__Card--Animation')
+  })
+}
+function RemoveClass(elements) {
+  elements.forEach(element => {
+    element.classList.remove('countdown__Card--Animation')
+  })
+}
+function CountChange(elements, count) {
+  elements.forEach(element => {
+    element.innerHTML = (count < 10) ? `0${count}` : count;
+  })
+}
+function timer(ms) {
+  return new Promise(res => setTimeout(res, ms))
+}
